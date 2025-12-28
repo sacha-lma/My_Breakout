8c3ecf97bda19133f4c880904dfdf8d8dbc0f428
Sacha Le Moign-Avalos <sacha.le-moign-avalos@epitech.eu>
2025-12-28 20:50:53 +0100

    Add horizontal movement boundary to player
    Introduced a maxX property to PlayerMovements and SampleScene to restrict player movement within horizontal bounds. Player can now only move within the range defined by maxX, preventing it from going off-screen.

--- My project/Assets/Scripts/PlayerMovements.cs
+++ My project/Assets/Scripts/PlayerMovements.cs
@@ -3,15 +3,20 @@ using UnityEngine;
 public class PlayerMovements : MonoBehaviour
 {
     public float speed;
-    float horizontalInput;
+    public float maxX;
+    private float _horizontalInput;
+    
     void Start()
     {
         
     }
 
-    void Update()
+    private void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        transform.position += Vector3.right * horizontalInput * speed * Time.deltaTime;
+        _horizontalInput = Input.GetAxis("Horizontal");
+        if (_horizontalInput > 0 && transform.position.x < maxX 
+            ||
+            _horizontalInput < 0 && transform.position.x > -maxX)
+            transform.position += Vector3.right * (_horizontalInput * speed * Time.deltaTime);
     }
 }
