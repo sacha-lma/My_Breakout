94ca5d2f2a6cbc4afd92809e786a1bd0ca11dfb5
Sacha Le Moign-Avalos <sacha.le-moign-avalos@epitech.eu>
2026-01-03 18:42:12 +0100

    Add new glyphs for 'F' and 'S' to Super Meatball SDF
    Updated the Super Meatball SDF font asset to include new glyphs for the characters 'F' and 'S', along with their metrics, glyph rects, and character table entries. Adjusted free glyph rects and atlas data to accommodate the new glyphs.

--- My project/Assets/Scripts/MainMenu/settings.cs
+++ My project/Assets/Scripts/MainMenu/settings.cs
@@ -1,17 +1,49 @@
 using System;
+using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class settings : MonoBehaviour
 {
     public AudioMixer masterMixer;
+    public TMP_Dropdown resolutionDropdown;
     
     bool MasterToggle = true;
     bool SfxToggle = true;
     bool MusicToggle = true;
     float MasterVolume;
+    Resolution[] resolutions;
+    private void Start()
+    {
+        List<String> options = new List<string>();
+        int currentResolutionIndex = 0;
+        
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
     
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
     private void Update()
     {
         if (!MasterToggle)
@@ -74,5 +106,10 @@ public class settings : MonoBehaviour
     public void SetQuality(int qualityIndex)
     {
         QualitySettings.SetQualityLevel(qualityIndex);
+    }
+    
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
     }
 }
